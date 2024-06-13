0fb316a9dc3dc3db8ce55d33b7415d6de8fa9594
Dario Fresi <dario.fresi@gmail.com>
2024-06-13 23:47:14 +0200

    Revision 1.1 - 13/06/2024

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,67 @@ import { CartaPagamento } from "./moduli-di-progetto/carta-pagamento";
     const utente3 = new Utente('Franco', 'Bianchi', 'franco.bianchi@gmail.com', barcellona);
 
 
+// PROVA DI FUNZIONAMENTO 
 
+    console.log('Aggiungo alcuni mezzi alla città di Londra')
+    console.log('');
+
+    // Aggiunta di mezzi ad una città a scelta
+
+    londra.aggiungiMezzo(mezzo1);
+    londra.aggiungiMezzo(mezzo2);
+    londra.aggiungiMezzo(mezzo6);
+    londra.aggiungiMezzo(mezzo7);
+    londra.aggiungiMezzo(mezzo11);
+    londra.aggiungiMezzo(mezzo12);
+
+    console.log('');
+    console.log(`L'utente1 visualizza la lista dei mezzi assegnati alla città`);
+
+    console.log('');
+    utente1.visualizzaMezzi();
+
+    console.log('');
+    console.log('utente1 (Dario) prova a prenotare un mezzo senza aver inserito un metodo di pagaemnto valido');
+
+    console.log('');
+    utente1.prenotaMezzo(mezzo1, 'mensile');
+
+    console.log('');
+    console.log('Ora utente1 registra un metodo di pagaemnto non valido (scaduto) e prenota il mezzo1');
+
+    utente1.inserisciMetodoPagamento(new CartaPagamento(1111222233334444, new Date(2020, 3)));
+    console.log('');
+    utente1.prenotaMezzo(mezzo1, 'mensile');
+
+    console.log('');
+    console.log('Ora utente1 inserisce un metodo di pagaemnto valido e riprova con la prenotazione del mezzo1');
+
+    utente1.inserisciMetodoPagamento(new CartaPagamento(1111333355557777, new Date(2027, 3)));
+    console.log('');
+    utente1.prenotaMezzo(mezzo1, 'mensile');
+
+    console.log('');
+    console.log(`L'utente2 registra una carta di credito e prova a prenotare il mezzo1 già prenotato da utente1`);
+
+    utente2.inserisciMetodoPagamento(new CartaPagamento(7777999900001111, new Date(2030, 9)));
+    console.log('');
+    utente2.prenotaMezzo(mezzo1, 'mensile');
+
+    console.log('');
+    console.log('utente 2 visualizza la lista aggiornata dei mezzi assegnati alla città');
+
+    utente2.visualizzaMezzi();
+
+    console.log('');
+    console.log(`L'utente1 rilascia il mezzo a sua disposizione`);
+
+    utente1.rilasciaMezzo();
+
+    console.log('');
+    console.log(`L'utente 2 richiede nuovamente la lista dei mezzi per controllare se il mezzo1 è adesso disponibile`);
+
+    utente2.visualizzaMezzi();
 
 
 
